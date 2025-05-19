eae4de2b3fc8513bfef34b8f3f7f3de9bb779d6e
Onujules <onuorajuliet2001@gmail.com>
2025-05-19 11:30:41 +0100

    Modify app in main branch

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 These include bug fixes in the JavaScript file that controls the weather data fetch, layout updates in the HTML file, and aesthetic improvements in the CSS file. You aim to commit these canges in a logical and organized manner.
+Modification in main branch.
 
